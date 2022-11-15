daf6efdca5da07aa50d59232aa65ed7fe6198c9f
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-15 16:40:07 -0500

    Embedding JavaScript expressions in JSX

--- my_first_react_app/src/App.js
+++ my_first_react_app/src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import Products from './Products';
 
 class App extends Component {
+
+  formatName(user){
+    return user.firstName + ' ' + user.lastName;
+  }
+
   render() {
+
+    // "user" is an object representting the user's first and last names.
+    const user = {
+      firstName: "Fabian Marcelo",
+      lastName: "Lopez Chamorro"
+    };
+
     return (
       <div>
-        <h1>My First React App!</h1>
+        <h1>Hello, {this.formatName(user)}</h1>
         <Products />
       </div>
     );
