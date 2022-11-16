aa09e06097462ddb1985a81029bf08863b75c8f8
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-15 22:01:12 -0500

    Calling the Rating component in App

--- my_first_react_app/src/App.js
+++ my_first_react_app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Products from './Products';
 import Button from 'react-bootstrap/Button';
+import Ratings from './Rating';
 
 class App extends Component {
 
@@ -23,6 +24,11 @@ class App extends Component {
         <h1>Hello, {this.formatName(user)}</h1>
         <Products />
         <Button variant="danger" disabled={!isValid}>Default</Button>
+        <Ratings rating="1"/>
+        <Ratings rating="2"/>
+        <Ratings rating="3"/>
+        <Ratings rating="4"/>
+        <Ratings rating="5"/>
       </div>
     );
   }
